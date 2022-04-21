0b538e7452948384dc6a27833f64be16294b389e
dieserNutzer <103006357+dieserNutzer@users.noreply.github.com>
2022-04-21 23:51:09 +0200

    Etwas Erklärung hinzugefügt.

--- ReinforcementAgent.java
+++ ReinforcementAgent.java
@@ -7,6 +7,19 @@ import massim.javaagents.MailService;
 
 import java.util.List;
 
+// kleiner Disclaimer: ich habe bisher nie als Programmierer gearbeitet, daher würde es mich nicht wundern, 
+// wenn euch mein Code etwas unbeholfen vorkommt.
+// Ich erkläre hier vorab wie der Agent arbeitet.
+// Das ganze basiert auf dem Buch "Reinforcement Learning An Introduction second edition" von Richard S. Sutton und Andrew G. Barto"
+
+// Weil es mehrere Arten von Reinforcementagenten gibt, ist die Klasse Reinforcement und SARSA getrennt.
+// Jeder input (bei Massim als "percept" bezeichent) ist eine Liste an Zahlen. Diese werden der Funktion "input" übergeben. 
+// Nach der Eingabe wird überprüft, ob der gegebene percept bereits bekannt ist, dies geschieht über die Funktion "checkForUnknownStates".
+// Wenn der percept neu ist wird er gespeichert und neue Wahrscheinlichkeiten für die Auswahl der Aktionen für diesen percept/state initialisiert, dies geschieht 
+// über die Funktion "addNewState".
+// In beiden Fällen wird der mit dem input übergebene Reward ausgewertet und die Wahrscheinlichkeiten für die Aktionen angepasst, dies geschieht über 
+// die Funktion "stateActionValueUpdate". Diese wird genauer in SARSA definiert, weil dies von der genauen Implementierung des ReinforcementLearnings abhängt.
+
 // übersetzt aus C++
 // ALLE Variablen hatten dort den Typ float oder std::vector<float>!
 // Das würde ich daher immer als erstes als Fehlerquelle in Betracht ziehen.
@@ -19,22 +32,22 @@ public abstract class ReinforcementAgent extends Agent{
 	float epsilon;						/*[eps-greedy = 1 - eps + (eps / anzahlActions), eps-non-greed = (eps / anzahlActions)] */
 	float discount;						/*schwächt rewards in entfernter Zukunft ab, wenn <1*/
 	float Reward;						/*der zum aktuellen Input gegebene reward*/
-	int Output;							/*Ergebnis des letzten chooseAction Aufrufs*/
+	int Output;						/*Index der gewählten Aktion (Ergebnis des letzten chooseAction Aufrufs)*/
 	// es scheint, so als müssten hier Conntainer-complatible Variablen für List genutzt werden.
 	// falls einer ne besser Variante hat als List<Integer> bzw. List<Float>, gerne umsetzen.
 	List<Float> states;					/*bekannte states (die bereits erlebten Inputs)*/
-	List<Float> stateActionValues;      /*die Wahrscheinlichkeiten der Actionen zu jedem state*/
+	List<Float> stateActionValues;      /*die Wahrscheinlichkeiten der Aktionen zu jedem state*/
 	// bei preferedPolicyIndex bin ich mir nicht ganz sicher, ob Int richtig ist.
-	List<Integer> preferedPolicyIndex;	/*die aktuell höchste Wahrscheinlichkeit aller aktionen eines bestimmten states, also argmax(stateActionValues[stateIndex bis stateIndex+inputDim])*/
-	List<Float> letzterInput;			/*der aktuelle Input oder auch state*/
+	List<Integer> preferedPolicyIndex;	/*die aktuell höchste Wahrscheinlichkeit aller Aktionen eines bestimmten states, also argmax(stateActionValues[stateIndex bis stateIndex+inputDim])*/
+	List<Float> letzterInput;			/*der aktuelle Input (ein state)*/
 	
 	public ReinforcementAgent(String name, MailService mailbox, 
 			int InDim, int outDim, float Epsilon, float Discount,
 			float LetzterReward, int LetzterActionIndex, List<Float> States, List<Float> sAV, 
 			List<Integer> pi, List<Float> LetzterInput ) {
 		super(name, mailbox);
-		this.epsilon= Epsilon;
-		this.discount = Discount;
+		epsilon= Epsilon;
+		discount = Discount;
 		inputDimension= InDim;
 		outputDimension=outDim;
 		states=States;
@@ -78,11 +91,12 @@ public abstract class ReinforcementAgent extends Agent{
 		int randomNum = ThreadLocalRandom.current().nextInt(0, (int)outputDimension);
 		preferedPolicyIndex.add((int)randomNum);	// bei neuen States wird geraten
 	}
-		
+	
 	public abstract float input(List<Float> input);
 	
 	abstract void stateActionValueUpdate();
 	
+	// Ich habe den Code vor Beginn meines Studiums begonnen und weiß mittlerweile, dass dies eine sehr naive Implementierung ist.
 	List<Integer> findNewStates (List<Float> knownStates, int stateDimensions,
 			List<Float> potentiallyNewStates /* muss die selbe Dimension pro Punkt vorweisen*/) {
 			int dimensions = stateDimensions;
